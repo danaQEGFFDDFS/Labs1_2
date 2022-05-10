359ef206b6ce294ec9a53ba6c18038d1ace37cac
danaQEGFFDDFS <99499448+danaQEGFFDDFS@users.noreply.github.com>
2022-05-10 20:45:41 +0300

    Add files via upload

---
+++ Lab2Test/Lab2Test/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace Lab2Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("http://www.omdbapi.com/?t="+textBox1.Text+"&apikey=460bf9b5"),
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+               textBox2.Text = body;
+            }
+
+        }
+
+      
+    }
+    
+}
