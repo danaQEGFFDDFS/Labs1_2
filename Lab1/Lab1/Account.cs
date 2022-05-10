9b5282ca4e646e67d9edb51d0c5f98551e61dfb9
danaQEGFFDDFS <99499448+danaQEGFFDDFS@users.noreply.github.com>
2022-05-10 20:44:12 +0300

    Add files via upload

---
+++ Lab1/Lab1/Account.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab1
+{
+    public class Account
+    {
+        [Key]
+        public int IdAcc { get; set; }
+        public int Balance { get; set; }
+        public string TypeAccount { get; set; }
+        
+        public DateTime DateNextPay { get; set; }
+       
+        [ForeignKey("IdUser")]
+        public virtual User User{ get; set; }
+
+    }
+}
---
+++ Lab1/Lab1/LabContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab1
+{
+    public class LabContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\Dana;Database=LAb1db;Trusted_Connection=True;");
+            }
+        }
+    }
+
